6fd67c61e12d7f0c3f083ef49f3ed5a39362f2c2
Pawel <20097569@mail.wit.ie>
2023-04-29 19:30:18 +0100

    Linted Main.kt file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    // Plugin for Dokka - KDoc generating tool
+    id("org.jetbrains.dokka") version "1.6.10"
     application
 }
 
@@ -15,9 +17,12 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("io.github.microutils:kotlin-logging:2.1.23")
-
+    implementation("org.slf4j:slf4j-simple:1.7.36")
     implementation("com.thoughtworks.xstream:xstream:1.4.18")
     implementation("org.codehaus.jettison:jettison:1.4.1")
+
+    // For generating a Dokka Site from KDoc
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.6.10")
 }
 
 tasks.test {
